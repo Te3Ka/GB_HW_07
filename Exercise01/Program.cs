bc5d74426b1a5366debaab9eb1e6946c24c34fbf
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-04 14:48:17 +0300

    Задание 1. Выполнено

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -20,6 +20,73 @@ void Author()
     Console.WriteLine();
 }
 
+//Печать массива на экран
+void PrintArray(float[,] arrayPrint)
+{
+    for (int i = 0; i < arrayPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayPrint.GetLength(1); j++)
+        {
+            Console.Write($"{arrayPrint[i,j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
 
+//Создание случайного двумерного массива
+float[,] CreateRandom2DArray(int m, int n, float min, float max)
+{
+    float[,] array2D = new float[m, n];
+    Random random = new Random();
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            array2D[i,j] = random.NextSingle() * (max - min) + min;
+        }
+    }
+    return array2D;
+}
+
+//Запрос на количество столбцов в массиве
+int GetColumnsLengthArray()
+{  
+    Console.Write("Сколько столбцов будет в массиве? ");
+    int.TryParse(Console.ReadLine(), out int num);
+    while (num <= 0)
+    {
+        Console.WriteLine("Число меньше 1! Массив не может быть создан. Введите заново: ");
+        int.TryParse(Console.ReadLine(), out num);
+    }
+    return num;
+}
+
+//Запрос на количество строк в массиве
+int GetRowsLengthArray()
+{  
+    Console.Write("Сколько строк будет в массиве? ");
+    int.TryParse(Console.ReadLine(), out int num);
+    while (num <= 0)
+    {
+        Console.WriteLine("Число меньше 1! Массив не может быть создан. Введите заново: ");
+        int.TryParse(Console.ReadLine(), out num);
+    }
+    return num;
+}
+
+//Получение минимума или максимума от пользователя
+float GetMinMax(string minOrMax)
+{
+    Console.Write($"Введите значение {minOrMax}: ");
+    float.TryParse(Console.ReadLine(), out float result);
+    return result;
+}
+
+Console.WriteLine("Программа генерирует массив из случайных вещественных чисел в количестве и диапазоне, которое вводит пользователь.");
+int m = GetColumnsLengthArray();
+int n = GetRowsLengthArray();
+float min = GetMinMax("минимума");
+float max = GetMinMax("максимума");
+PrintArray(CreateRandom2DArray(m, n, min, max));
 
 Author();
