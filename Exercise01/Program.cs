9d145f83e964d851fd13a0d1d11cb63fb3686a35
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-04 14:21:48 +0300

    Задание 1. Начало

---
+++ Exercise01/Program.cs
@@ -0,0 +1,25 @@
+﻿/***********************/
+/*****Te3Ka_PaynE*******/
+/*Mnement4813@yandex.ru*/
+/***********************/
+
+/*
+Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+//Метод с указанием автора
+void Author()
+{
+    Console.WriteLine();
+    Console.WriteLine("Программа создана Te3Ka_PaynE.");
+    Console.WriteLine("E-mail: Mnement4813@yandex.ru");
+    Console.WriteLine();
+}
+
+
+
+Author();
